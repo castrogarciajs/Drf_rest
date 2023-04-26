50e8b2c0c51527b6ad1071a47e13b01816211a71
Sebastian009w <ia3266595@gmail.com>
2023-04-26 18:57:06 -0500

    database url

--- drf_rest/settings.py
+++ drf_rest/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'drf_rest.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url(
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
