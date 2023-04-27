eab56796e0fcf4b0cf0989a143305e6b66761ffd
Sebastian009w <ia3266595@gmail.com>
2023-04-26 19:31:55 -0500

    cambios

--- drf_rest/settings.py
+++ drf_rest/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'drf_rest.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/postgres',
+        default='postgresql://postgres:postgres@localhost:5432/drfrest',
         conn_max_age=600
     )
 }
